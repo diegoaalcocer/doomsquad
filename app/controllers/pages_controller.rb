f8ce0318e81d533358dca25dd3ed8387ca333e6e
Diego Alcocer <diegoaalcocer@gmail.com>
2018-03-29 20:14:01 +0000

    Design and template integrated

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def podcast
+  end
+  
+  def services
+  end
+  
+  def store
+  end
+  
+  def videos
+  end
+end
